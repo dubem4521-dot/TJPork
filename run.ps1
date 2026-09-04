26ffe8f03758940f3ca12200f53f9188ed7db5c6
dubem4521-dot <dubem4521@gmail.com>
2026-09-04 16:44:48 +0200

    add comments to run.ps1 and pull remote updated files

--- run.ps1
+++ run.ps1
@@ -1,11 +1,11 @@
-# Stop and remove any existing containers
+# Stop and remove any existing containers.
 docker stop tjpork; docker rm tjpork
 
-# Build a new image from the Dockerfile
+# Build a new image from the Dockerfile.
 docker build -t tjpork-app -f src/TJPork.Web/Dockerfile .
 
-# Run the new container
+# Run the new container.
 docker run -d -p 8080:80 --name tjpork tjpork-app
 
-# check if the container is running
+# check if the container is running.
 docker ps -a
